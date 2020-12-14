5489c1f4bddbb0fc4d0ec789fb7b08b3cff3097b
keneishac <keneishacorbett@gmail.com>
2020-12-14 15:40:21 -0800

    adds updates to services

--- src/services/crochet.js
+++ src/services/crochet.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001/api/crochet/'
+const BASE_URL = 'https://crochetcorner-backend.herokuapp.com/crochet'
 
 function fetchCrochetData(){
     return fetch(BASE_URL).then(res => res.json())
