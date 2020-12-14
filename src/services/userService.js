a0c626542b881d4ea4d8dff4254e3cfbe8ee0cf1
keneishac <keneishacorbett@gmail.com>
2020-12-14 15:26:56 -0800

    updates user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
 
-const BASE_URL = 'http://localhost:3001/api/users';
+const BASE_URL = 'https://crochetcorner-backend.herokuapp.com/api/users';
 
 
 
